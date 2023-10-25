1028fdbd2c493e3d924e3c2f6c636ff3c39a0e19
k-zakaria <kacimizakaria28@gmail.com>
2023-10-25 16:10:32 +0100

    j'ai termine menu burger

---
+++ index.js
@@ -0,0 +1,7 @@
+//---------------------------------menu burgre-------------------------------------
+const menuhome = document.querySelector('.header__menuhome');
+ const nav = document.querySelector('nav');
+
+ menuhome.addEventListener('click' , function() {
+    nav.classList.toggle('opne');
+ });    
